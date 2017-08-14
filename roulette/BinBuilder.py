86e2905bc3e5dd0f0c089dee8488e7635529d7e0
rosslogan702 <rosslogan702@gmail.com>
2017-08-14 21:27:30 +0100

    Adding binbuilder class and test

---
+++ roulette/BinBuilder.py
@@ -0,0 +1,9 @@
+"""
+Description: BinBuilder class that builds the various permutations of outcomes in the bins on the wheel
+Author: Ross Logan 2017
+"""
+
+class BinBuilder(object):
+
+    def __init__(self):
+        pass
---
+++ tests/test_binBuilder.py
@@ -0,0 +1,14 @@
+"""
+Description: Test class for BinBuilder
+Author: Ross Logan 2017
+"""
+
+from unittest import TestCase
+
+class TestBinBuilder(TestCase):
+
+    def setUp(self):
+        pass
+
+    def testBuildBins(self):
+        pass
