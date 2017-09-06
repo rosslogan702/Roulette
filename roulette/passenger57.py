9647eb01b8052ff0a80b5173e6fe424c81ec0a0c
rosslogan702 <rosslogan702@gmail.com>
2017-09-06 20:14:16 +0100

    Adding logic for passenger57 stub class

---
+++ roulette/passenger57.py
@@ -0,0 +1,25 @@
+"""
+Description: Passenger 57 class that always bets on black, this will be replaced in future with Player class
+             No Unit Testing will be added for this, as it is going to be replaced in the future
+Author: Ross Logan 2017
+"""
+from roulette.outcome import Outcome
+from roulette.bet import Bet
+
+class PassengerFiftySeven(object):
+
+    def __init__(self, table):
+        self.table = table
+        self.black = Outcome("BLACK", 1)
+
+    def placeBets(self):
+        bet = Bet(2, self.black)
+        self.table.placeBet(bet)
+
+    def win(self, bet):
+        print("The following bet won: " + str(bet))
+
+    def lose(self, bet):
+        print("The following bet lost: " + str(bet))
+
+
