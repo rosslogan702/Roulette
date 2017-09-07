8bbcbb31fd18807d9ebe9368edbf9b9f67a267d4
rosslogan702 <rosslogan702@gmail.com>
2017-09-07 20:17:49 +0100

    Adding roulette game and demo runner

---
+++ roulette/demo/demorunner.py
@@ -0,0 +1,22 @@
+"""
+Description: Class to perform a demonstration of the roulette game class
+Author: Ross Logan 2017
+"""
+from roulette.wheel import Wheel
+from roulette.nonrandom import NonRandom
+from roulette.passenger57 import PassengerFiftySeven
+from roulette.table import Table
+from roulette.roulettegame import RouletteGame
+
+class DemoRunner(object):
+
+    def __iter__(self):
+        self.wheel = Wheel(NonRandom())
+        self.table = Table()
+        self.player = PassengerFiftySeven(self.table)
+        self.game = RouletteGame(self.wheel, self.table)
+
+    def runGame(self):
+        for cycle in range(0,3):
+            self.game.cycle(self.player)
+
---
+++ roulette/roulettegame.py
@@ -0,0 +1,20 @@
+"""
+Description:    Roulette Game class that manages the sequences of actions that defines a game of Roulette
+Author:         Ross Logan 2017
+"""
+
+class RouletteGame(object):
+
+    def __init__(self, wheel, table):
+        self.wheel = wheel
+        self.table = table
+
+    def cycle(self, player):
+        player.placeBets()
+        nextWinningBin = self.wheel.next()
+
+        for bet in self.table.bets:
+            if bet.outcome in nextWinningBin.outcomes:
+                player.win(bet)
+            player.lose(bet)
+
